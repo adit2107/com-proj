a7e7983c25757caa9d7b6ce64c493d21718ce7a6
adit2107 <adit.ratnam@gmail.com>
2020-07-10 12:49:36 +0530

    Add routing and withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 
+import {Route, Switch} from 'react-router-dom';
+
 import { HomePage } from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <HomePage />
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
+    </div>
+   
   );
 }
 
