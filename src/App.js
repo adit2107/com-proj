b8b76906b2d353ff7b2ecf58da4c582d413e658c
adit2107 <adit.ratnam@gmail.com>
2020-07-14 19:04:39 +0530

    Add sign in, custom button component, fix styling

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import './App.css';
 import {Route, Switch} from 'react-router-dom';
 
 import { HomePage } from './pages/homepage/homepage.component';
-
 import ShopPage from './pages/shop-page/shoppage.component';
-
-import Header from './components/header/header.component'
+import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component.jsx';
 
 function App() {
   return (
@@ -16,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signIn' component={SignInAndSignUp}/>
       </Switch>
     </div>
    
